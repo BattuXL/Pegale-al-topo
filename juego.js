4af3e93d33c4d463befb89aeae5d8adc6d83009c
YafetFz <159202688+YafetFz@users.noreply.github.com>
2024-07-16 21:21:49 -0600

    Add files via upload

--- juego.js
+++ juego.js
@@ -1,5 +1,6 @@
-
 document.addEventListener("DOMContentLoaded", function() {
+
+    //Esta seccion captura los elementos necesarios del html como: topos, el puntaje y las vidas
     const topos = document.querySelectorAll(".topo");
     const to1 = document.querySelector(".to1");
     const to2 = document.querySelector(".to2");
@@ -11,11 +12,24 @@ document.addEventListener("DOMContentLoaded", function() {
     const to8 = document.querySelector(".to8");
     const to9 = document.querySelector(".to9");
     const to10 = document.querySelector(".to10");
-    const contador = document.querySelector(".puntos")
+    const contador = document.querySelector(".puntos");
+    const vidas = document.querySelector(".vidas");
+
+    //Definimos las variables que se van a utilizar: musica, contador para sumar el puntaje, el restador de vidas, la velocidad y el sonido del golpe
     let num = 0;
     let cont = 0;
-    let velocidad = 4000;
+    let vid = 4;
+    let velocidad = 4500;
+    let musica = new Audio('audio/gameplay.mpeg');
+    let golpe = new Audio('audio/golpe.mp3');
+    let hit = false;
 
+    //Reproducimos la musica del juego
+    musica.play();
+    musica.loop = true;
+    musica.volume = 0.2;
+
+    //Esta funcion sirve para ocultar los topos
     function esconder() {
         to1.style.display = "none";
         to2.style.display = "none";
@@ -29,10 +43,25 @@ document.addEventListener("DOMContentLoaded", function() {
         to10.style.display = "none";
     }
 
+    //Esta funcion sirve para mostrar los topos de forma aleatoria segun el agujero y dependiendo si no se logro golpear a un topo se restaran las vidas 
     function mostrar() {
+        if (!hit) {
+            vid--;
+            vidas.textContent = `Vidas: ${vid}`;
+            if (vid === 0) {
+                alert("Fin del juego\nTu puntaje fue: " + cont);
+                esconder();
+                musica.pause();
+                return;
+            }
+        }
+
+
+        //Aqui llamamos a la funcion esconder para ocultar los topos en caso de no haber golpeado uno de ellos
         esconder();  
         num = Math.floor(Math.random() * 10) + 1;
         
+        //Aqui se muestra el topo dependiendo del numero aleatorio que se genere del 1 al 10 
         if (num == 1) {
             to1.style.display = "flex";
         } else if (num == 2) {
@@ -55,24 +84,33 @@ document.addEventListener("DOMContentLoaded", function() {
             to10.style.display = "flex";
         }
 
+        hit = false;
+
+        //Aqui se utiliza un setTimeout que, cada cierto tiempo la funcion se llama asi misma y la variable "velocidad" se disminuye para aumentar la velocidad de aparicion de los topos
         setTimeout(mostrar, velocidad);
     }
 
+    //Aqui se agrega el evento de click a cada topo para que al presionar el topo se aumente el puntaje y se reproduce el sonido del golpe, oculte al topo y aumente su velocidad llamando a la funcion "aumentar()"
     topos.forEach(topo => {
         topo.addEventListener("mousedown", function() {
             esconder();
             aumentar();
             contador.textContent = `Puntaje: ${++cont}`;
+            golpe.play();
+            golpe.volume = 0.12;
+            hit = true;
         });
     });
 
+    //Funcion para aumentar la velocidad de aparicion de los topos entre mas topos se hayan golpeado, teniendo un limite de 1000
     function aumentar() {
         velocidad -= 500;
-        if (velocidad < 1000) {
+        if (velocidad <= 1000) {
             velocidad = 1000;
         }
-        console.log(velocidad);
     }
 
+    //Funcion que inicia el juego basicamente 
     mostrar();
 });
+
